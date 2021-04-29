cc33e4fee9d7703894c0ceef982dfdf04b60fe1f
oleh <peretjatkool@gmail.com>
2021-04-29 16:24:38 +0300

    change GetUserById, create GetUserByIdRequest

--- routes/api/user.php
+++ routes/api/user.php
@@ -1,6 +1,8 @@
 <?php
 use \App\Http\Controllers\User\UserController;
+
+
 Route::get('/contract_address', 'PublicApiController@address');
 Route::get('/all', 'UserController@getAllUsers');
 Route::get('/wallet/{address}', [UserController::class, 'getUserByWallet']);
-Route::get('/{id}', 'UserController@getUserById');
+Route::get('/{id}', [UserController::class, 'getUserById']);

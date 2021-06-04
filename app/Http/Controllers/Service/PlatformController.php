c3554e2c624f241adae8d4a9d7f12c3f8b19047b
oleh <peretjatkool@gmail.com>
2021-06-04 19:06:34 +0300

    change request PlatformController.php method platformUsersInfo only were activated platforms

--- app/Http/Controllers/Service/PlatformController.php
+++ app/Http/Controllers/Service/PlatformController.php
@@ -78,7 +78,7 @@ class PlatformController extends Controller
 
     public function platformUsersInfo(Wallet $wallet)
     {
-        return PlatformResource::collection($wallet->platforms()->orderBy('id')->get());
+        return PlatformResource::collection($wallet->platforms()->orderBy('id')->get()->unique('platform_level_id'));
     }
 
     public function platformReactivationUsersInfo(Wallet $wallet)

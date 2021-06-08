d85d7cb1cd667d7212bd36315b5f5d50c5235621
oleh <peretjatkool@gmail.com>
2021-06-08 19:38:13 +0300

    add activated_platforms to UserSubscriberResource

--- app/Http/Resources/User/UserSubscriberResource.php
+++ app/Http/Resources/User/UserSubscriberResource.php
@@ -9,18 +9,20 @@ class UserSubscriberResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
+     *
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'id'               => $this->id,
-            'contract_user_id' => $this->contract_user_id,
-            'referrals_count'  => $this->subscribers()->count(),
-            'wallet'           => $this->wallet()->select('id', 'address', 'amount_transfers')->get(),
-            'platforms'        => $this->wallet->platforms()->get()->count(),
-            'created_at'       => $this->created_at,
+            'id'                  => $this->id,
+            'contract_user_id'    => $this->contract_user_id,
+            'referrals_count'     => $this->subscribers()->count(),
+            'wallet'              => $this->wallet()->select('id', 'address', 'amount_transfers')->get(),
+            'platforms'           => $this->wallet->platforms()->get()->count(),
+            'activated_platforms' => $this->wallet->platforms()->where('activated', true)->get()->count(),
+            'created_at'          => $this->created_at,
         ];
     }
 }

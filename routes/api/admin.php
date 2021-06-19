b981c2009a4905e99b181cab8e69ebb99de0c607
oleh <peretjatkool@gmail.com>
2021-06-20 02:06:42 +0300

    add middleware to video routes

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -2,7 +2,7 @@
 
 Route::post('change-password', [\App\Http\Controllers\Admin\Auth\ChangePasswordController::class, 'store']);
 Route::apiResource('languages', 'LanguageController');
-Route::apiResource('videos', 'VideoController');
+Route::apiResource('videos', 'VideoController')->middleware(['auth:admins']);
 Route::post('login', [\App\Http\Controllers\Admin\Auth\AuthController::class, 'login']);
 Route::post('registration', [\App\Http\Controllers\Admin\Auth\RegistrationController::class, 'registration'])->middleware(['auth:wallet']);
 Route::match(['get', 'post'], 'switcher-2fa', [\App\Http\Controllers\Admin\Auth\AuthController::class, 'switcher2FA']);
